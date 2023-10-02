b19f7f8dd1ba938d287bd35ea9e1afcc49f0f6f0
Shubham Tiwari <shubht3303@gmail.com>
2023-10-02 18:38:14 +0530

    Web Page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, Kaise ho Saare , Shubham Tiwari here , <br/> I Eat Sleep Code Chant Read Scriptures and Repeat
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/DevJSter"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github-Profile  
         </a>
       </header>
     </div>
